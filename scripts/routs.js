aecaca499b740ac0c4082435ff78d8200a6ceaf8
sumanbh <sumanbh7@gmail.com>
2016-07-21 17:23:58 -0600

    fixed routes

--- scripts/routs.js
+++ scripts/routs.js
@@ -6,27 +6,27 @@ angular.module('faronBig')
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: '../views/home.html'
+                templateUrl: './views/home.html'
             })
             .state('story', {
                 url: '/pages/our-story',
-                templateUrl: '../views/ourStory.html'
+                templateUrl: './views/ourStory.html'
             })
             .state('projects', {
                 url: '/pages/projects',
-                templateUrl: '../views/projects.html'
+                templateUrl: './views/projects.html'
             })
             .state('shop', {
                 url: '/pages/shop',
-                templateUrl: '../views/shop.html'
+                templateUrl: './views/shop.html'
             })
             .state('login', {
                 url: '/account/login',
-                templateUrl: '../views/login.html'
+                templateUrl: './views/login.html'
             })
             .state('error', {
                 url: '/404',
-                templateUrl: '../views/error404.html'
+                templateUrl: './views/error404.html'
             });
 
 
