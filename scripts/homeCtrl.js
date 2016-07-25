ac8fb93882289aaa9eed9b6bfa5642ee24657457
sumanbh <sumanbh7@gmail.com>
2016-07-24 23:21:31 -0600

    more css fixes; removed redundent js

--- scripts/homeCtrl.js
+++ scripts/homeCtrl.js
@@ -1,20 +1,13 @@
 var app = angular.module('faronBig');
 
 app.controller('homeCtrl', function(){
-  
+
 })
 
 app.directive('imgSlide', function() {
     return {
         link: function(scope, element, attr){
-          element.slider().css({
-            'height': '42vw',
-            'min-height': '300px'
-          });
-           element.find(".slides").css({
-             'height': '42vw',
-             'min-height': '300px'
-           });
+          element.slider()
       }
     };
 });
