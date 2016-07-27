5739c3683fc08cf375923d70381b26df8a7b39b6
sumanbh <sumanbh7@gmail.com>
2016-07-26 22:25:38 -0600

    changed mailchimp font size

--- scripts/homeCtrl.js
+++ scripts/homeCtrl.js
@@ -1,13 +1,13 @@
 var app = angular.module('faronBig');
 
-app.controller('homeCtrl', function(){
-  
+app.controller('homeCtrl', function() {
+
 })
 
 app.directive('imgSlide', function() {
     return {
-        link: function(scope, element, attr){
+        link: function(scope, element, attr) {
           element.slider();
-      }
+        }
     };
 });
